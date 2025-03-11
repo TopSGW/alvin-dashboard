8068ceb5037a9e7d2b131d37909da47f92695f16
TopSGW <web.smartdev22@gmail.com>
2025-03-10 21:49:22 -0700

    change port to 3000

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,10 @@ module.exports = {
   apps: [{
     name: "admin-dashboard",
     script: "npm",
-    args: "run preview",
+    args: "run preview -- --port 3000",
     env: {
       NODE_ENV: "production",
+      PORT: 3000
     },
   }]
 };
